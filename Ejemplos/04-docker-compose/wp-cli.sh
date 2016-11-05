1b97b5dd5ef21c8a59a06e24d030b6ff253a4462
Alejandro Madrigal Leiva <me@alemadlei.tech>
2016-11-05 12:43:50 -0600

    Ejecutables

--- Ejemplos/04-docker-compose/wp-cli.sh
+++ Ejemplos/04-docker-compose/wp-cli.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-# Utiliza WP CLI para activar el plugin y el tema de ejemplos.
-docker exec $1 wp plugin activate wordcampcr2016_plugin --allow-root
-docker exec $1 wp theme install _tk --allow-root
-docker exec $1 wp theme activate wordcampcr2016_theme --allow-root
+../03-Dockerfile-desarrollo/wp-cli.sh
